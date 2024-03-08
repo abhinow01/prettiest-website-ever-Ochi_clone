7663708febc73aef59eeb4872a65e2f12b011e6c
abhinav khanna <kabhinav99@gmail.com>
2024-03-08 12:49:04 +0530

    landing page

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -4,13 +4,19 @@ import { FaArrowUp } from "react-icons/fa6";
 const LandingPage = () => {
   return (
     <div className='w-full h-screen bg-zinc-900 pt-1'>
-        <div className='textstructure mt-52 px-20'>  
-       {["We Create","Eye Opening","Presentations"].map((item,i)=>(
-       <div className="masker" key={i}>
-       <h1 className="uppercase text-[7.5vw] leading-[7vw] tracking-tighter font-medium ">{item}</h1>
-       </div>
-       ))}               
-        </div>
+       <div className='textstructure mt-52 px-20'>
+  {["We Create", "Eye Opening", "Presentations"].map((item, i) => (
+    <div className="masker" key={i}>
+      <div className='w-fit flex items-end overflow-hidden'>
+        {i === 1 && (
+          <div className='mr-[1vw] w-[8vw] h-[6vw] rounded-md relative bg-green-500 -top-[0.5vw]' ></div>
+        )}
+        <h1 className="uppercase text-[8vw] leading-[7vw] tracking-tighter font-['Founders Grotesk X-Condensed'] font-bold">{item}</h1>
+      </div>
+    </div>
+  ))}
+</div>
+
         <div className='border-t-[1px] border-zinc-700 mt-32 flex justify-between items-center py-5 px-20'>
          {["For Public and private companies","From the first pitch to IPO"].map((element,i)=>(
             <p className='text-md font-light tracking-tight leading-none' key={i}>{element}</p>
@@ -19,8 +25,7 @@ const LandingPage = () => {
             <div className='px-5 py-2 border-[1px] border-zinc-500 font-light text-md uppercase rounded-full '> start the project</div>
                 <div className='w-10 h-10 flex items-center justify-center border-[1px] border-zinc-500 rounded-full'>
                 <span className='rotate-[45deg]'><FaArrowUp /></span>
-                </div>
-                
+                </div>     
          </div>
         </div>
         
